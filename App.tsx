852068108d7867c4725d2e696833ee8985359fb6
Радмир Карманов <karr@soft-servis.local>
2023-04-10 09:44:45 +0500

    installed navigation stack

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Main from './src/Main';
+import Settings from './src/Settings';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <Main />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Main} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
