b77599a7e804c938938499c76d97d17ba8c9ce96
Radmir <radmir_96@mail.ru>
2023-04-11 15:05:07 +0500

    add modal window and change amount of winners. And added visibility button and modal.

--- src/Winner.jsx
+++ src/Winner.jsx
@@ -5,19 +5,34 @@ import {
   View,
   useWindowDimensions,
   TouchableOpacity,
+  Modal,
+  TextInput,
+  KeyboardAvoidingView,
 } from 'react-native';
 import {DATA_STORAGE_KEY, getData} from './functions';
 
 const Winner = () => {
   const {styles} = useStyle();
   const [data, setData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
+  const [amount, setAmount] = useState('');
+
+  const handleNameInput = (text) => {
+    setAmount(text);
+  };
+
+  const handleSave = () => {
+    setIsModalVisible(false);
+  };
 
   const getRandom = async () => {
+    setIsButtonVisible(false);
     const data = await getData(DATA_STORAGE_KEY);
 
     let result = [];
 
-    while (result.length != 3) {
+    while (result.length != amount) {
       const random = Math.floor(Math.random() * data.length);
       result.push(data[random]);
       result = result.filter((v, i, arr) => arr.indexOf(v) == i);
@@ -30,11 +45,31 @@ const Winner = () => {
 
   return (
     <View style={styles.wrapper}>
-      <TouchableOpacity style={styles.btn} onPress={getRandom}>
-        <Text style={styles.btnText}>Разыграть</Text>
-      </TouchableOpacity>
-      {data.map((item) => (
-        <Text style={styles.text} key={item.id}>
+      <View>
+        {isModalVisible && (
+          <Modal onRequestClose={() => setIsModalVisible(false)}>
+            <KeyboardAvoidingView style={styles.wrapper}>
+              <Text style={styles.text}>Введите количество победителей</Text>
+              <TextInput
+                style={styles.input}
+                value={amount}
+                keyboardType="numeric"
+                onChangeText={handleNameInput}
+              />
+              <TouchableOpacity style={styles.btn} onPress={handleSave}>
+                <Text style={styles.btnText}>Сохранить</Text>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>
+          </Modal>
+        )}
+      </View>
+      {isButtonVisible && (
+        <TouchableOpacity style={styles.btn} onPress={getRandom}>
+          <Text style={styles.btnText}>Разыграть</Text>
+        </TouchableOpacity>
+      )}
+      {data.map((item, index) => (
+        <Text style={styles.text} key={index}>
           {item.name}: {item.phone}
         </Text>
       ))}
@@ -76,6 +111,12 @@ const useStyle = () => {
       fontSize: width * 0.03,
       color: '#fff',
       marginTop: 50,
+    },
+    input: {
+      backgroundColor: '#fff',
+      margin: 10,
+      borderRadius: 10,
+      width: '50%',
     },
   });
   return {styles};
